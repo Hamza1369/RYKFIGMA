f41c80df43e7fa9ba6ae0da5e20cf6aa5ff8a5de
Hamza1369 <117157904+Hamza1369@users.noreply.github.com>
2024-08-15 19:01:15 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
 
     <Routes>
+      <Route path='/' element={<Homepage />} />
 
       <Route path='/pervious' element={<Homepage />} />
       <Route path='/next' element={<Nextpg />} />
